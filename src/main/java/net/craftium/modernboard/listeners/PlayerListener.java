be48ad39991c8c5436b68dc63b188883b6ee479d
Artuto <artuto@artuto.xyz>
2023-12-27 00:24:43 -0600

    Cleanup on player logoff

--- src/main/java/net/craftium/modernboard/listeners/PlayerListener.java
+++ src/main/java/net/craftium/modernboard/listeners/PlayerListener.java
@@ -1,11 +1,10 @@
 package net.craftium.modernboard.listeners;
 
 import net.craftium.modernboard.ModernBoard;
-import net.craftium.modernboard.wrappers.Scoreboard;
-import net.kyori.adventure.text.Component;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerListener implements Listener
 {
@@ -19,11 +18,12 @@ public class PlayerListener implements Listener
     @EventHandler(ignoreCancelled = true)
     public void onPlayerJoin(PlayerJoinEvent event)
     {
-        Scoreboard sb = plugin.getScoreboardManager().addPlayer(event.getPlayer());
-        sb.title(Component.text("TECH TEST"))
-                .line(0, Component.text("Line 1"))
-                .line(1, Component.text("Line 2"))
-                .line(2, Component.text("Line 3"))
-                .line(3, Component.text("Line 4"));
+        plugin.getScoreboardManager().addPlayer(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event)
+    {
+        plugin.getScoreboardManager().removePlayer(event.getPlayer());
     }
 }
